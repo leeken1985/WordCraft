2ff7f75fdaee0802c14312c0d8fb85ea2df0ff6f
rareyes <rhea.reyes15@gmail.com>
2013-02-21 18:13:40 -0800

    changed keyboard presses in Mario

--- TestGame2.0/TestGame2.0/TestGame2.0/Mario.cs
+++ TestGame2.0/TestGame2.0/TestGame2.0/Mario.cs
@@ -16,12 +16,14 @@ namespace TestGame2._0
         const int START_Y_POSITION = 0;
         const int MOVE_UP = -1;
         const int MOVE_DOWN = 1;
-        const int MOVE_LEFT = -1;
-        const int MOVE_RIGHT = 1;
+        const int MOVE_LEFT = -10;
+        const int MOVE_RIGHT = 10;
         const int SPEED = 160;
 
         Vector2 speed = Vector2.Zero;
         Vector2 direction = Vector2.Zero;
+
+        KeyboardState oldState = Keyboard.GetState();
 
         public void LoadContent(ContentManager theContentManager)
         {
@@ -33,26 +35,42 @@ namespace TestGame2._0
             base.LoadContent(theContentManager, "mariosprite");
         }
 
-        private void UpdateMovement(KeyboardState aCurrentKeyboardState)
+        private void UpdateMovement(KeyboardState newState)
         {
-            speed = Vector2.Zero;
-            direction = Vector2.Zero;
-             if (aCurrentKeyboardState.IsKeyDown(Keys.Left) == true)
+             speed = Vector2.Zero;
+             direction = Vector2.Zero;
+             newState = Keyboard.GetState();
+             //Move Left one key press at a time
+             if (newState.IsKeyDown(Keys.Left))
              {
-                 speed.X = SPEED;
-                 direction.X = MOVE_LEFT;
+                 if (!oldState.IsKeyDown(Keys.Left))
+                 {
+                     speed.X = SPEED;
+                     direction.X = MOVE_LEFT;
+                 }
              }
-             else if (aCurrentKeyboardState.IsKeyDown(Keys.Right) == true)
+             else if (newState.IsKeyDown(Keys.Left))
              {
-                 speed.X = SPEED;
-                 direction.X = MOVE_RIGHT;
              }
+             //Move right one key press at a time
+             else if (newState.IsKeyDown(Keys.Right))
+             {
+                 if (!oldState.IsKeyDown(Keys.Right))
+                  {
+                      speed.X = SPEED;
+                      direction.X = MOVE_RIGHT;
+                  }
+             }
+             else if (newState.IsKeyDown(Keys.Right))
+             {
+             }
+             oldState = newState;
         }
 
         public void Update(GameTime gameTime)
         {
-            KeyboardState aCurrentKeyboardState = Keyboard.GetState();
-            UpdateMovement(aCurrentKeyboardState);
+            KeyboardState newState = Keyboard.GetState();
+            UpdateMovement(newState);
             base.Update(gameTime, speed, direction);
         }
         
