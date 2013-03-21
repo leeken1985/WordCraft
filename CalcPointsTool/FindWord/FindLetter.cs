3c3cba2dfcc5c1c2b325b106d88f82e028c4a526
dklau <derek.lau@shaw.ca>
2013-03-21 15:57:27 -0700

    Updated tools

---
+++ CalcPointsTool/FindWord/FindLetter.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace FindWord
+{
+    public partial class FindLetter : Form
+    {
+        Dictionary<string, int> wordMap = new Dictionary<string, int>();
+
+        public FindLetter()
+        {
+            InitializeComponent();
+           
+            wordMap.Add("best", 0);
+            wordMap.Add("beer", 0);
+            wordMap.Add("bear", 0);
+            wordMap.Add("beat", 0);
+            wordMap.Add("beet", 0);
+        }
+
+        private void findWord()
+        {
+            String line = textBox1.Text;
+
+            // index of letter that was fired
+		    int findex = 2;
+
+		    List<string> words = new List<string>();
+
+		    String erb = "";
+		
+		    switch(findex){
+		    case 0:
+			    erb = line.Substring(0, 4);
+			    words.Add(erb);
+			    break;
+		    case 1:
+			    erb = line.Substring(1, 4);
+			    words.Add(erb);
+			    erb = line.Substring(2, 4);
+			    words.Add(erb); 
+			    break;
+		    case 2:
+			    erb = line.Substring(0, 4);
+			    words.Add(erb);
+			    erb = line.Substring(1, 4);
+			    words.Add(erb);
+			    erb = line.Substring(2, 4);
+			    words.Add(erb);
+			    break;
+		    case 3:
+			    erb = line.Substring(0, 4);
+			    words.Add(erb);
+			    erb = line.Substring(1, 4);
+			    words.Add(erb);
+			    erb = line.Substring(2, 4);
+			    words.Add(erb);
+			    break;
+		    case 4:
+			    erb = line.Substring(1, 4);
+			    words.Add(erb);
+			    erb = line.Substring(2, 4);
+			    words.Add(erb);
+			    break;
+		    case 5: 
+			    erb = line.Substring(3, 4);
+			    words.Add(erb);
+			    break;
+		    } 
+
+          foreach(string s in words){
+              if (wordMap.ContainsKey(s))
+              {
+                  label2.Text = s;
+              }
+		    }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            findWord();
+        }
+    }
+}
---
+++ CalcPointsTool/RandomLetter/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace RandomLetter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void generateLetter()
+        {
+            List<char> list = new List<char>();
+
+            for (int i = 0; i < 12; i++)
+            {
+                list.Add('e');
+            }
+            for (int i = 0; i < 9; i++)
+            {
+                list.Add('a');
+                list.Add('i');
+            }
+            for (int i = 0; i < 8; i++)
+            {
+                list.Add('o');
+            }
+            for (int i = 0; i < 6; i++)
+            {
+                list.Add('n');
+                list.Add('r');
+                list.Add('t');
+            }
+            for (int i = 0; i < 4; i++)
+            {
+                list.Add('l');
+                list.Add('s');
+                list.Add('u');
+                list.Add('d');
+            }
+            for (int i = 0; i < 3; i++)
+            {
+                list.Add('g');
+            }
+            for (int i = 0; i < 2; i++)
+            {
+                list.Add('b');
+                list.Add('c');
+                list.Add('m');
+                list.Add('p');
+                list.Add('f');
+                list.Add('h');
+                list.Add('v');
+                list.Add('w');
+                list.Add('y');
+            }
+
+            list.Add('k');
+            list.Add('j');
+            list.Add('x');
+            list.Add('q');
+            list.Add('z');
+
+            Random random = new Random();
+            int result = random.Next(list.Count);
+
+            textBox1.Text = list[result] + "";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            generateLetter();
+        }
+
+    }
+}
