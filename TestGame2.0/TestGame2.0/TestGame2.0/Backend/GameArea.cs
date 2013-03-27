18ec46eefeb81aa966ef2244bf7ffaa2b0b0b0d0
Team Bdark <bcitprojects2013@gmail.com>
2013-03-27 09:39:40 -0700

    Starting to implement a gamebaord class

---
+++ TestGame2.0/TestGame2.0/TestGame2.0/Backend/GameArea.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace TestGame2._0.Backend
+{
+    //TODO: Implement gamePiece class
+    class GameArea 
+    {
+        private const int areaWidth = 8; //8 columns
+        private const int areaHeight = 14; //14 rows
+        private GamePiece[,] boardSquares = new GamePiece[areaWidth, areaHeight]; //board squares
+        
+        public GameArea()
+        {
+            //clear game area.
+            ClearArea();
+        }
+
+        public void ClearArea()
+        {
+            for (int x = 0; x < areaWidth; x++)
+            {
+                for (int y = 0; y < areaHeight; y++)
+                {
+                    //initialize entire board area to nothing
+                    boardSquares[x, y] = new GamePiece(""); 
+                }
+            }
+        }
+    }
+}
---
+++ TestGame2.0/TestGame2.0/TestGame2.0/Backend/GamePiece.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace TestGame2._0.Backend
+{
+    class GamePiece
+    {
+        private const int blockWidth = 50;
+        private const int blockHeight = 50;
+        private string value;
+
+        public GamePiece(string value)
+        {
+
+        }
+    }
+}
